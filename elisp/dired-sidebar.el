9ff3d25b13e47f8c6efbd0247897d5d7e0e12f54
Cerek Hillen <cerekh@gmail.com>
2020-01-25 23:50:08 -0500

    Explore directories in sidebar + more hotkeys for projectile

--- elisp/dired-sidebar.el
+++ elisp/dired-sidebar.el
@@ -1,2 +1,7 @@
+(require 'dired)
+
 ;; Adding a global hotkey to toggle the dired-sidebar.
 (global-set-key (kbd "C-c d") 'dired-sidebar-toggle-sidebar)
+
+;; Toggling directories in dired sidebar
+(bind-key (kbd "TAB") 'dired-subtree-toggle dired-mode-map)
