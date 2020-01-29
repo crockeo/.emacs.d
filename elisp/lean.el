a40ed36ff1826ab2f1e62113f2cc4011695f97af
Cerek Hillen <cerekh@gmail.com>
2020-01-29 12:29:09 -0500

    Adding company to lean

--- elisp/lean.el
+++ elisp/lean.el
@@ -1,8 +1,11 @@
+(require 'company)
+(require 'company-lean)
+
 ;; Opening the lean error and goal windows when we open a lean file.
 (defun open-lean-windows ()
   (defvar original-buffer)
   (setq original-buffer (current-buffer))
-  
+
   ;; Setting up our windows with those buffers live
   (select-window (split-window-right 120))
   (switch-to-buffer "*Lean Goal*")
@@ -13,4 +16,12 @@
   (other-window 1)
   (switch-to-buffer original-buffer))
 
-(add-hook 'lean-mode-hook 'open-lean-windows)
+;; Setting up Lean
+(defun setup-lean ()
+  ;; Opening error and goal windows by default
+  (open-lean-windows)
+
+  ;; Adding lean to the list of company backends
+  (add-to-list 'company-backends 'company-lean))
+
+(add-hook 'lean-mode-hook 'setup-lean)
