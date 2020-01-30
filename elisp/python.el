819f031a485bca13d111aa2542cc6aebc7f5d4f0
Cerek Hillen <cerekh@gmail.com>
2020-01-29 21:50:52 -0500

    Customizing indentation in Python because it's...bad otherwise

--- elisp/python.el
+++ elisp/python.el
@@ -2,16 +2,8 @@
 (require 'elpy)
 (require 'jedi-core)
 (require 'projectile)
+(require 'python)
 (require 'pyvenv)
-
-;;;;
-;; TODO:
-;;   * Indentation
-;;     * Make python-mode indent only one block, instead of 2 blocks, when in a
-;;       function declaration.
-;;
-;;     * Make python-mode indent to the last indentation level inside of a
-;;       multi-line string.
 
 ;; Installing elpy dependencies in the virtualenv.
 (defun setup-venv-elpy ()
@@ -56,9 +48,35 @@
   (add-to-list 'company-backends 'company-jedi)
 
   ;; Configuring black
-  (ensure-black))
+  (ensure-black)
+
+  ;; Keeping Python mode from rebinding my precious RET
+  (define-key python-mode-map (kbd "RET") 'newline-and-indent))
 
 (add-hook 'python-mode-hook 'setup-python-mode)
+
+;; Forcing python-mode to opinionated indentation.
+(defun custom-indentation (orig-fun &rest args)
+  (save-excursion
+    (pcase (python-indent-context)
+      ;; When we make a new line in a docstring, we want to move to the
+      ;; same indentation level.
+      (`(:inside-docstring . ,start)
+       (goto-char start)
+       (current-indentation))
+
+      ;; When we define a new function, we only want to be one block indented
+      ;; from the function definition, not two.
+      (`(:inside-paren-newline-start-from-block . ,start)
+       (goto-char start)
+       (+ (current-indentation) python-indent-offset))
+
+      ;; If we are not in an overriden context, execute the standard function.
+      (_ (apply orig-fun args)))))
+
+(advice-add
+ 'python-indent-calculate-indentation
+ :around #'custom-indentation)
 
 ;; Configuring jedi to work within elpy.
 (setq jedi:complete-on-dot t)
