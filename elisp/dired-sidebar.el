f0ce20dbf720440f70f8eb04093a975691baca2e
Cerek Hillen <cerekh@gmail.com>
2020-01-31 21:42:51 -0500

    Rm dired sidebar

--- elisp/dired-sidebar.el
+++ elisp/dired-sidebar.el
@@ -2,3 +2,10 @@
 
 ;; Toggling directories in dired sidebar
 (define-key dired-mode-map (kbd "TAB") 'dired-subtree-toggle)
+
+;; Removing line numbers when we're in dired-sidebar
+(defun disable-line-numbers (&rest ignore)
+  (message "testing")
+  (display-line-numbers-mode -1))
+
+(add-hook 'dired-mode-hook 'disable-line-numbers)
