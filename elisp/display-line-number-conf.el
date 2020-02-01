21652a812e02d5b3d4f5e9a61a6915461bfdeeb6
Cerek Hillen <cerekh@gmail.com>
2020-02-01 10:14:02 -0500

    oops

---
+++ elisp/display-line-number-conf.el
@@ -0,0 +1,9 @@
+;; Explorations in rendering text on the left-hand-side.
+
+
+;; Forcing line numbers to be a consistent width.
+(defvar display-line-numbers-width-start)
+(setq display-line-numbers-width-start 1)
+
+;; Adding enabling line number by default
+(global-display-line-numbers-mode)
