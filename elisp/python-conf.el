e124841b40cce58a18f98a63b8e4e63340a46140
Cerek Hillen <cerekh@gmail.com>
2020-02-21 22:07:30 -0800

    pared down python conf

--- elisp/python-conf.el
+++ elisp/python-conf.el
@@ -1,35 +1,13 @@
 ;; Installing packages
 (use-package python-mode)
-
 (use-package blacken)
-
-(use-package elpy)
-
-(use-package jedi)
-
-(use-package company-jedi
-  :after jedi company)
-
-(require 'company)
-(require 'jedi-core)
-(require 'projectile)
+(use-package pyvenv)
 
 ;; NOTE: If you're getting a jediepcserver error, then make a Python2 virtual
 ;;       environment:
 ;;
 ;;   - rm -rf ~/.emacs.d/.python-environments/default
 ;;   - virtualenv -p ~/.emacs.d/.python-environments/default
-
-(load-library "jedi-posframe")
-
-;; Installing elpy dependencies in the virtualenv.
-(defun setup-venv-elpy ()
-  (apply 'start-process
-         "setup-venv-elpy" "setup-venv-elpy"
-         "pip"
-         "install"
-         "--upgrade"
-         (elpy-rpc--get-package-list)))
 
 ;; Automatically switches to the venv in a Python directory if it exists.
 (defun setup-venv ()
@@ -56,54 +34,11 @@
 
 ;; Setting company-jedi with company-mode
 (defun setup-python-mode ()
-  ;; Setting up out virtualenv
   (setup-venv)
-
-  ;; Launching jedi
-  (elpy-mode)
-  (jedi-mode)
-  (add-to-list 'company-backends 'company-jedi)
-
-  ;; Making jedi render with posframe
-  (jedi-posframe-mode)
-
-  ;; Removing highlight-indentation-mode from elpy
-  (highlight-indentation-mode 0)
-
-  ;; Configuring black
   (ensure-black)
-
-  ;; Keeping Python mode from rebinding my precious RET
   (define-key python-mode-map (kbd "RET") 'newline-and-indent))
 
 (add-hook 'python-mode-hook 'setup-python-mode)
-
-;; Forcing python-mode to opinionated indentation.
-(defun custom-indentation (orig-fun &rest args)
-  (save-excursion
-    (pcase (python-indent-context)
-      ;; When we make a new line in a docstring, we want to move to the
-      ;; same indentation level.
-      (`(:inside-docstring . ,start)
-       (goto-char start)
-       (current-indentation))
-
-      ;; When we define a new function, we only want to be one block indented
-      ;; from the function definition, not two.
-      (`(:inside-paren-newline-start-from-block . ,start)
-       (goto-char start)
-       (+ (current-indentation) python-indent-offset))
-
-      ;; If we are not in an overriden context, execute the standard function.
-      (_ (apply orig-fun args)))))
-
-(advice-add
- 'python-indent-calculate-indentation
- :around #'custom-indentation)
-
-;; Configuring jedi to work within elpy.
-(setq jedi:complete-on-dot t)
-(setq jedi:get-in-function-call-delay 250)
 
 ;; Skipping string normalization for Black.
 (setq blacken-skip-string-normalization t)
