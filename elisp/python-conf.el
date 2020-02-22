5cebdf182e6a22de031b30e5be7642e71a6c9219
Cerek Hillen <cerekh@gmail.com>
2020-02-21 22:10:21 -0800

    Add back opinionated indentation

--- elisp/python-conf.el
+++ elisp/python-conf.el
@@ -40,5 +40,28 @@
 
 (add-hook 'python-mode-hook 'setup-python-mode)
 
+;; Forcing python-mode to opinionated indentation.
+(defun custom-indentation (orig-fun &rest args)
+  (save-excursion
+    (pcase (python-indent-context)
+      ;; When we make a new line in a docstring, we want to move to the
+      ;; same indentation level.
+      (`(:inside-docstring . ,start)
+       (goto-char start)
+       (current-indentation))
+
+      ;; When we define a new function, we only want to be one block indented
+      ;; from the function definition, not two.
+      (`(:inside-paren-newline-start-from-block . ,start)
+       (goto-char start)
+       (+ (current-indentation) python-indent-offset))
+
+      ;; If we are not in an overriden context, execute the standard function.
+      (_ (apply orig-fun args)))))
+
+(advice-add
+ 'python-indent-calculate-indentation
+ :around #'custom-indentation)
+
 ;; Skipping string normalization for Black.
 (setq blacken-skip-string-normalization t)
