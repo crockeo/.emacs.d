c443ad1fbb66b70e34198189e64b86b46988de55
Cerek Hillen <chillen@lyft.com>
2021-07-27 13:42:55 -0700

    garbage collect + add libexec path

--- lisp/pkgs/builtin.el
+++ lisp/pkgs/builtin.el
@@ -11,10 +11,21 @@
 	read-process-output-max (* 1024 1024 4) ;; 2MB
 	ring-bell-function 'ignore)
 
-  (run-with-idle-timer
-   1 t
-   (lambda ()
-     (garbage-collect-maybe 3)))
+  ;; TODO: do something similar for linux
+  (when (string= system-type "darwin")
+    (dolist (path '("~/.emacs.d/emacs/nextstep/Emacs.app/Contents/MacOS/libexec"
+		    "~/.emacs.d/emacs/nextstep/Emacs.app/Contents/MacOS/bin"))
+      (push (expand-file-name path)
+	    exec-path)))
+
+  (defun ch/builtin/garbage-collect ()
+    (let ((original-gc-cons-threshold gc-cons-threshold))
+      (setq gc-cons-threshold (* 1024 1024)) ;; 1MB
+      (garbage-collect)
+      (setq gc-cons-threshold original-gc-cons-threshold)))
+
+  (add-hook 'focus-out-hook #'ch/builtin/garbage-collect)
+  (run-with-idle-timer 1 t #'ch/builtin/garbage-collect)
 
   (global-auto-revert-mode 1)
   (global-display-line-numbers-mode 1)
