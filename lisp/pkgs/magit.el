bfd9013410e8b6bd201c11bae2c7a14caf1980fa
Cerek Hillen <chillen@lyft.com>
2021-08-19 10:31:49 -0700

    improve remote detection again :)

--- lisp/pkgs/magit.el
+++ lisp/pkgs/magit.el
@@ -1,17 +1,21 @@
 ;;; magit.el -*- lexical-binding: t; -*-
 
 (ch/pkg magit
+  (defun ch/magit/visit-pr/get-repo-name (remote)
+    (replace-regexp-in-string
+     "\\(git@github\\.com:\\|\\.git$\\)"
+     ""
+     remote))
+
+  (ch/magit/visit-pr/get-repo-name "git@github.com:lyft/piptools.git")
+
   (defun ch/magit/visit-pr ()
     (interactive)
     (browse-url
-     (concat
-      "https://github.com/"
-      (replace-regexp-in-string
-       "git@github\\.com:"
-       ""
-       (magit-get "remote" (magit-get-remote "master") "url"))
-      "/pull/new/"
-      (magit-get-current-branch))))
+     (concat "https://github.com/"
+	     (ch/magit/visit-pr/get-repo-name (magit-get "remote" "origin" "url"))
+	     "/pull/new/"
+	     (magit-get-current-branch))))
 
   (use-package magit
     :config
