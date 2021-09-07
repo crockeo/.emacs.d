e72d894947d5d74167693f9cdd6c523a55e7ac5c
Cerek Hillen <chillen@lyft.com>
2021-09-06 18:23:08 -0700

    change olivetti mode to only trigger on split window

--- lisp/pkgs/olivetti.el
+++ lisp/pkgs/olivetti.el
@@ -1,13 +1,25 @@
 ;;; olivetti.el -*-; lexical-binding: t; -*-
 
 (ch/pkg olivetti
+  (defun ch/olivetti/all-horizontal ()
+    (car (and (mapcar #'window-full-width-p (window-list)))))
+
+  (defun ch/olivetti/toggle-visible-windows (mode-toggle)
+    (mapc
+     (lambda (window)
+       (with-current-buffer (window-buffer window)
+	 (olivetti-mode mode-toggle)))
+     (window-list)))
+
   (defun ch/olivetti/auto-olivetti (window)
-    (if (= (length (window-list)) 1)
-	(olivetti-mode 1)
-      (olivetti-mode -1)))
+    (let ((mode-toggle (if (ch/olivetti/all-horizontal)
+			   1
+			 -1)))
+      (ch/olivetti/toggle-visible-windows mode-toggle)))
 
   (use-package olivetti
     :config
-    (setq olivetti-minimuma-body-width 120))
+    (setq olivetti-minimum-body-width 240))
 
+  ;; (remove-hook 'window-size-change-functions #'ch/olivetti/auto-olivetti)
   (add-hook 'window-size-change-functions #'ch/olivetti/auto-olivetti))
