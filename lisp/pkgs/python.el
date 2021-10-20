877ab51646db25ddf6c3a3f251484359aa2395f9
Cerek Hillen <chillen@lyft.com>
2021-10-20 10:48:31 -0700

    fix python indentation

--- lisp/pkgs/python.el
+++ lisp/pkgs/python.el
@@ -22,4 +22,24 @@
     (setq lsp-pyright-log-level "error"
 	  lsp-pyright-multi-root nil))
 
-  (use-package pyvenv))
+  (use-package pyvenv)
+
+  (defun ch/python/indentation-override (orig-fun &rest args)
+    (save-excursion
+      (pcase (python-indent-context)
+	;; when inside of a function definition, e.g.
+	;;
+	;; def func(
+	;;     argument: str
+	;; ): -> str:
+	;;     pass
+	;;
+	;; do not try to indent argument two indentation widths
+	;; instead just prefer one!
+	(`(:inside-paren-newline-start-from-block . ,start)
+	 (goto-char start)
+	 (+ (current-indentation) python-indent-offset))
+
+	(_ (apply orig-fun args)))))
+
+  (advice-add 'python-indent--calculate-indentation :around #'ch/python/indentation-override))
