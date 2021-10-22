494e60534a15df186abc210dc11326ad3877fe5a
Cerek Hillen <cerekh@gmail.com>
2021-10-21 20:36:29 -0700

    further javascript configuration

--- lisp/pkgs/javascript.el
+++ lisp/pkgs/javascript.el
@@ -2,6 +2,22 @@
 
 (ch/pkg javascript
   (use-package typescript-mode)
+  (use-package web-mode
+    :mode (("\\.tsx?\\'" . web-mode))
+    :config
+    (setq web-mode-code-indent-offset 2))
 
-  (add-hook 'javascript-mode-hook #'lsp-deferred)
-  (add-hook 'typescript-mode-hook #'lsp-deferred))
+  (defun ch/javascript/configure-indentation ()
+    (setq indent-tabs-mode nil)
+    (setq tab-width 2)
+    (setq js-indent-level 2)
+    (setq typescript-indent-level 2))
+
+  (defun ch/javascript/on-load ()
+    (ch/javascript/configure-indentation)
+    (lsp-deferred))
+
+  ;; (add-to-list 'auto-mode-alist '(".tsx\\'" . typescript-mode))
+
+  (add-hook 'js-mode-hook #'ch/javascript/on-load)
+  (add-hook 'typescript-mode-hook #'ch/javascript/on-load))
