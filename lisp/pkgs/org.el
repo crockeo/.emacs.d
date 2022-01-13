7571f86c88b470cbf768e477d7f8759da57cfb0f
Cerek Hillen <cerekh@gmail.com>
2022-01-12 19:28:38 -0800

    better error handling

--- lisp/pkgs/org.el
+++ lisp/pkgs/org.el
@@ -8,18 +8,28 @@
 
   (defun ch/org/push-files ()
     (interactive)
+    (message "org.el: Pushing files...")
     (ch/org/ensure-org-directory)
     (make-thread
      (lambda ()
-       (ch/under-dir org-directory
-	 (magit-git "add" ".")
-	 (magit-git "commit" "-m" (format "%s -- syncing org files" (current-time-string)))
-	 (magit-git "push" "-u" "origin" (magit-get-current-branch))))))
+       (condition-case ()
+	   (progn
+	     (ch/under-dir org-directory
+	       (call-process "git" nil nil nil "add" ".")
+	       (call-process "git commit" "-m" (format "%s -- syncing org files" (current-time-string)))
+	       (call-process "git push" "-u" "origin" (magit-get-current-branch)))
+	     (message "org.el: Pushed files!"))
+	 (error (message "org.el: Failed to push files."))))))
 
   (defun ch/org/pull-files ()
     (interactive)
+    (message "org.el: Pulling files...")
     (ch/org/ensure-org-directory)
     (make-thread
      (lambda ()
-       (ch/under-dir org-directory
-	 (magit-git "pull" "origin" (magit-get-current-branch)))))))
+       (condition-case ()
+	   (progn
+	     (ch/under-dir org-directory
+	       (call-process "git" nil nil nil "pull" "origin" (magit-get-current-branch)))
+	     (message "org.el: Pulled files!"))
+	 (error (message "org.el: Failed to push files.")))))))
