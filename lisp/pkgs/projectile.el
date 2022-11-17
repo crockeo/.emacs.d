89e559fe256209738a755b2913da96c0d813a745
Cerek Hillen <cerekh@gmail.com>
2022-11-17 11:49:27 -0800

    projectile things

--- lisp/pkgs/projectile.el
+++ lisp/pkgs/projectile.el
@@ -1,4 +1,30 @@
 (ch/pkg projectile
+  ;; TODO: actually use this :)
+  ;; it's a faster way of doing project discovery because it stops after the first .git
+  ;; but i don't know how to put it into projectile :(
+  (defun ch/projectile/project-dirs (&optional root-dir depth)
+    (cl-block project-dirs
+      (let* ((depth (or depth 0))
+	     (root-dir (or root-dir (expand-file-name "~/src"))))
+	(when (> depth 3)
+	 (cl-return-from project-dirs nil))
+
+	(let ((project-dirs nil))
+	  (cl-dolist (file (directory-files root-dir))
+	    (when (string= file ".git")
+	      (cl-return-from project-dirs (list root-dir)))
+
+	    (unless (or (string= file ".") (string= file ".."))
+	      (message "%s" file)
+	      (let ((qualified-name (concat root-dir "/" file)))
+		(when (file-directory-p qualified-name)
+		  (setf project-dirs
+			(append project-dirs
+				(ch/projectile/project-dirs qualified-name
+							    (+ depth 1))))))))
+
+	  project-dirs))))
+
   (use-package projectile
     :init
     (setq projectile-project-search-path
