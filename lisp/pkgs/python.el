5a4917373197302362812fe6db2fd1271a1edddc
Cerek Hillen <cerekh@gmail.com>
2022-12-08 14:20:20 -0800

    shh

--- lisp/pkgs/python.el
+++ lisp/pkgs/python.el
@@ -1,15 +1,6 @@
 ;;; python.el -*- lexical-binding: t; -*-
 
 (ch/pkg python
-  (defun ch/python/lyftvenv ()
-    (interactive)
-
-    (let ((default-directory (projectile-project-root)))
-      (when default-directory
-	(with-temp-buffer
-	  (start-process "lyftvenv" "*lyftvenv*"
-			 "lyftvenv")))))
-
   (use-package lsp-pyright
     :init (setq lsp-pyright-multi-root nil)
     :hook (python-mode . (lambda ()
