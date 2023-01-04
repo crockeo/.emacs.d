42250f2aee95f6c4c502561a58a168cd73abec0d
Cerek Hillen <cerekh@gmail.com>
2023-01-04 06:49:32 -0800

    set eglot config to report errors on hover

--- lisp/pkgs/eglot.el
+++ lisp/pkgs/eglot.el
@@ -3,4 +3,14 @@
 (ch/pkg eglot
   (use-package eglot)
 
+  (defun ch/eglot/config ()
+    (setq eldoc-documentation-functions
+	  (cons #'flymake-eldoc-function
+		(remove #'flymake-eldoc-function eldoc-documentation-functions))
+
+	  eldoc-documentation-strategy
+	  #'eldoc-documentation-default))
+
+  (add-hook 'eglot-managed-mode-hook #'ch/eglot/config)
+
   )
